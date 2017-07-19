be6aa650a9037adbfe64d0ad2b1a155069155609
thirtytwoten <thirtytwoten3210@gmail.com>
2017-07-19 09:59:59 -0400

    tabs to spaces

--- www/js/client.js
+++ www/js/client.js
@@ -7,51 +7,51 @@ var position = 'TL';
 var angle = 0;
 
 socket.on('addFinger', function(finger){
-	game.addFinger(finger.name, finger.position, finger.angle, finger.isLocal);
+  game.addFinger(finger.name, finger.position, finger.angle, finger.isLocal);
 });
 
 socket.on('sync', function(gameServerData){
-	game.receiveData(gameServerData);
+  game.receiveData(gameServerData);
 });
 
 socket.on('killTurtle', function(turtleData){
-	game.killTurtle(turtleData);
+  game.killTurtle(turtleData);
 });
 
 socket.on('removeTurtle', function(turtleId){
-	game.removeTurtle(turtleId);
+  game.removeTurtle(turtleId);
 });
 
 $(document).ready( function(){
 
-	$('#join').click( function(){
-		name = $('#finger-name').val();
-		joinGame(name, position, angle, socket);
-	});
+  $('#join').click( function(){
+    name = $('#finger-name').val();
+    joinGame(name, position, angle, socket);
+  });
 
-	$('#finger-name').keyup( function(e){
-		name = $('#finger-name').val();
-		var k = e.keyCode || e.which;
-		if(k == 13){
-			joinGame(name, position, angle, socket);
-		}
-	});
+  $('#finger-name').keyup( function(e){
+    name = $('#finger-name').val();
+    var k = e.keyCode || e.which;
+    if(k == 13){
+      joinGame(name, position, angle, socket);
+    }
+  });
 
-	// $('ul.turtle-selection li').click( function(){
-	// 	$('.turtle-selection li').removeClass('selected')
-	// 	$(this).addClass('selected');
-	// 	selectedTurtle = $(this).data('turtle');
-	// });
+  // $('ul.turtle-selection li').click( function(){
+  //  $('.turtle-selection li').removeClass('selected')
+  //  $(this).addClass('selected');
+  //  selectedTurtle = $(this).data('turtle');
+  // });
 
 });
 
 $(window).on('beforeunload', function(){
-	socket.emit('leaveGame', name);
+  socket.emit('leaveGame', name);
 });
 
 function joinGame(name, position, angle, socket){
-	if(name != ''){
-		$('#prompt').hide();
-		socket.emit('joinGame', {name: name, position: position, angle: angle});
-	}
+  if(name != ''){
+    $('#prompt').hide();
+    socket.emit('joinGame', {name: name, position: position, angle: angle});
+  }
 }
